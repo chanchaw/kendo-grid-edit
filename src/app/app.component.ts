11bc10f291f1ba35a143a60548f4b28fe4a1f5fc
chanchaw <chan_chaw@126.com>
2020-02-01 12:39:32 +0800

    修改选中行

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GridDataResult, SelectableSettings } from '@progress/kendo-angular-grid';
+import { State,process } from '@progress/kendo-data-query';
+import { Product } from './model';
+import { EditService } from './edit.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,106 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'grideditor';
+
+  public view: Observable<GridDataResult>;
+  public gridState: State = {
+      sort: [],
+      skip: 0,
+      take: 10
+  };
+
+  public editDataItem: Product;
+  public isNew: boolean;
+  // private editService: EditService;
+
+
+
+  // constructor(@Inject(EditService) editServiceFactory: any) {
+  //     this.editService = editServiceFactory();
+  // }
+
+
+  public checkboxOnly = false;
+  public mode = 'single';
+  public selectableSettings: SelectableSettings;
+  public mySelection: string[] = [];
+  public selectedRow:Product = new Product();
+  public selectedAssets:any = {};
+  
+  constructor(
+    private editService:EditService
+  ) {
+    // this.editService = editServiceFactory();
+    this.setSelectableSettings();
+  }
+
+  public setSelectableSettings(): void {
+    this.selectableSettings = {
+        checkboxOnly: this.checkboxOnly,
+        mode: 'single'
+    };
+}
+
+
+  public ngOnInit(): void {
+      this.view = this.editService.pipe(map(data => process(data, this.gridState)));
+
+      this.editService.read();
+  }
+
+  public onStateChange(state: State) {
+      this.gridState = state;
+
+      this.editService.read();
+  }
+
+  public addHandler() {
+      this.editDataItem = new Product();
+      this.isNew = true;
+  }
+
+  public editHandler({dataItem}) {
+      // if( dataItem === undefined || dataItem === null ){
+      //   this.editDataItem = this.selectedRow;
+      // }else{
+      //   this.editDataItem = dataItem;
+      // }
+      this.editDataItem = dataItem;
+      this.isNew = false;
+  }
+
+  public cancelHandler() {
+      this.editDataItem = undefined;
+  }
+
+  public saveHandler(product: Product) {
+      this.editService.save(product, this.isNew);
+
+      this.editDataItem = undefined;
+  }
+
+  public removeHandler({dataItem}) {
+      this.editService.remove(dataItem);
+  }
+
+
+  
+
+  onButtonClick(){
+    // console.log(this.mySelection);
+    // console.log(this.editService.getSelectedRow(this.mySelection[0]));
+    // this.editHandler(this.editService.getSelectedRow(this.mySelection[0]));
+
+  }
+
+  onClickRow(){
+    alert('点击了行');
+  }
+
+
+  onCellClick(e){
+    console.log(e);
+    this.selectedRow = e.dataItem;
+    this.selectedAssets = e;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { EditFormComponent } from './edit-form/edit-form.component';
+
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
+import { HttpClientModule,HttpClientJsonpModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GridModule,
+    BrowserAnimationsModule,
+    ButtonsModule,FormsModule,ReactiveFormsModule, DialogsModule,HttpClientModule,HttpClientJsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
